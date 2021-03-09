e46910ffa1d66b6dba60adb3c9d8174a0805e720
Elliot Levin <elliot.levin@cheproximity.com.au>
2021-03-09 22:28:23 +1100

    remove unused imports

--- src/eth/ip_forwarder.rs
+++ src/eth/ip_forwarder.rs
@@ -1,12 +1,13 @@
-use std::{net::{IpAddr, Ipv4Addr}, sync::mpsc::Sender, thread, time::Duration};
+use std::{
+    net::{IpAddr, Ipv4Addr},
+    sync::mpsc::Sender,
+};
 
-use anyhow::Result;
-use pnet::packet::ipv4::{Ipv4Packet, MutableIpv4Packet};
+use pnet::packet::ipv4::Ipv4Packet;
 use pnet::{
-    packet::Packet,
     datalink::NetworkInterface,
-    packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket},
-    util::MacAddr,
+    packet::ethernet::{EthernetPacket, MutableEthernetPacket},
+    packet::Packet,
 };
 use state::{Client, State};
 
@@ -14,7 +15,12 @@ use crate::state::{self, SharedState};
 
 use super::event::Event;
 
-pub fn process_packet(tx: &mut Sender<Event>, state: &mut SharedState, eth: EthernetPacket, interface: &NetworkInterface) {
+pub fn process_packet(
+    tx: &mut Sender<Event>,
+    state: &mut SharedState,
+    eth: EthernetPacket,
+    interface: &NetworkInterface,
+) {
     let ip = Ipv4Packet::new(eth.payload()).unwrap();
     log::trace!("packet is ipv4");
 
@@ -33,11 +39,15 @@ pub fn process_packet(tx: &mut Sender<Event>, state: &mut SharedState, eth: Ethe
         return;
     }
 
-    log::trace!("received packet from {} destined for {} in lan", source_mac, dest_ip);
+    log::trace!(
+        "received packet from {} destined for {} in lan",
+        source_mac,
+        dest_ip
+    );
 
     let clients = state.get(|state| {
         let source_client = state.clients.iter().find(|i| i.mac == Some(source_mac));
-        
+
         if source_client.is_none() {
             log::trace!("could not find client with mac {}", source_mac);
             return None;
@@ -54,7 +64,11 @@ pub fn process_packet(tx: &mut Sender<Event>, state: &mut SharedState, eth: Ethe
         let dest_client = dest_client.unwrap();
         let next_hop_client = find_next_hop_client(state, source_client, dest_client);
 
-        Some((source_client.clone(), dest_client.clone(), next_hop_client.clone()))
+        Some((
+            source_client.clone(),
+            dest_client.clone(),
+            next_hop_client.clone(),
+        ))
     });
 
     if clients.is_none() {
@@ -63,25 +77,40 @@ pub fn process_packet(tx: &mut Sender<Event>, state: &mut SharedState, eth: Ethe
 
     let (source_client, dest_client, next_hop_client) = clients.unwrap();
 
-    log::debug!("forwarding packet from {:?} to {:?} via next hop {:?}", source_client, dest_client, next_hop_client);
+    log::debug!(
+        "forwarding packet from {:?} to {:?} via next hop {:?}",
+        source_client,
+        dest_client,
+        next_hop_client
+    );
     send_packet_to_next_hop(tx, next_hop_client, interface, eth);
 }
 
 fn is_in_local_net(dest_ip: Ipv4Addr, interface: &NetworkInterface) -> bool {
-    interface.ips.iter()
+    interface
+        .ips
+        .iter()
         .any(|i| i.contains(IpAddr::V4(dest_ip)))
 }
 
 /// Returns the next client along the chain.
 /// This will return a client which is one stop closer to the destination along the chain.
 /// The chain is defined by the order at which the appear in the Vec<Client>
-fn find_next_hop_client<'a>(state: &'a State, source_client: &'a state::Client, dest_client: &'a state::Client) -> &'a Client  {
+fn find_next_hop_client<'a>(
+    state: &'a State,
+    source_client: &'a state::Client,
+    dest_client: &'a state::Client,
+) -> &'a Client {
     if source_client == dest_client {
         log::debug!("source client is equal to dest client, looping back");
         return dest_client;
     }
 
-    let source_index = state.clients.iter().position(|c| c == source_client).unwrap();
+    let source_index = state
+        .clients
+        .iter()
+        .position(|c| c == source_client)
+        .unwrap();
     let dest_index = state.clients.iter().position(|c| c == dest_client).unwrap();
 
     let next_hop_index = if source_index < dest_index {
@@ -93,14 +122,25 @@ fn find_next_hop_client<'a>(state: &'a State, source_client: &'a state::Client, 
     return &state.clients[next_hop_index];
 }
 
-fn send_packet_to_next_hop(tx: &mut Sender<Event>, next_hop: Client, interface: &NetworkInterface, eth: EthernetPacket) {
+fn send_packet_to_next_hop(
+    tx: &mut Sender<Event>,
+    next_hop: Client,
+    interface: &NetworkInterface,
+    eth: EthernetPacket,
+) {
     if interface.mac.is_none() {
-        log::warn!("could not forward packet on interface {} as mac is not known", interface.name);
+        log::warn!(
+            "could not forward packet on interface {} as mac is not known",
+            interface.name
+        );
         return;
     }
 
     if next_hop.mac.is_none() {
-        log::warn!("could not forward packet to next hop {} as mac is not known", next_hop.name);
+        log::warn!(
+            "could not forward packet to next hop {} as mac is not known",
+            next_hop.name
+        );
         return;
     }
 
